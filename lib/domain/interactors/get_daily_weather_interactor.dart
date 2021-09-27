a173637b7aebd6b8b49ec5f8e6c8b3f0f9b03346
Yuliia Koba <yuliia.koba@nure.ua>
2021-09-27 16:51:48 +0300

    add interactors

---
+++ lib/domain/interactors/get_daily_weather_interactor.dart
@@ -0,0 +1,15 @@
+import 'package:weather_app/domain/interactors/base_interactor.dart';
+import 'package:weather_app/domain/models/coordinates.dart';
+import 'package:weather_app/domain/models/daily_weather.dart';
+import 'package:weather_app/domain/repositories/dailyWeatherRepository.dart';
+
+class GetDailyWeatherInteractor
+    implements BaseInteractor<Coordinates, List<DailyWeather>> {
+  GetDailyWeatherInteractor(this._repository);
+  final DailyWeatherRepository _repository;
+
+  @override
+  Future<List<DailyWeather>> call(Coordinates coordinates) {
+    return _repository.getDailyWeather(coordinates);
+  }
+}

9eb695958a044ab14204ee1e2a7c55eff4d10d1b
Yuliia Koba <yuliia.koba@nure.ua>
2021-09-29 02:56:28 +0300

    add splash screen

--- lib/src/weather_app.dart
+++ lib/src/weather_app.dart
@@ -1,6 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:weather_app/src/features/home_page/home_page.dart';
+import 'package:weather_app/src/features/splash_screen/splash_screen.dart';
 
 class WeatherApp extends StatelessWidget {
   const WeatherApp({Key? key}) : super(key: key);
@@ -11,7 +11,7 @@ class WeatherApp extends StatelessWidget {
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
-      home: HomePage(),
+      home: SplashScreen(),
     );
   }
 }

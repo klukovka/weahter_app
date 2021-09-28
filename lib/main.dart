9083c4c7ea219f07cd09c14095e3f7cebaf526f0
Yuliia Koba <yuliia.koba@nure.ua>
2021-09-29 02:14:00 +0300

    add only portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 import 'package:flutter/material.dart';
 import 'package:weather_app/src/weather_app.dart';
@@ -6,12 +7,13 @@ import 'package:weather_app/src/resources/translations/codegen_loader.g.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await EasyLocalization.ensureInitialized();
   await DotEnv.dotenv.load(fileName: '.env');
   runApp(
     EasyLocalization(
       path: 'assets/translations',
-      supportedLocales: [        
+      supportedLocales: [
         Locale('en'),
         Locale('ru'),
       ],

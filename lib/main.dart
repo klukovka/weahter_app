c97d36be0343a93e7525e55a4a030eb37e4acb28
Yuliia Koba <yuliia.koba@nure.ua>
2021-09-29 04:25:02 +0300

    add FCM

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,52 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as dot_env;
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:weather_app/src/weather_app.dart';
 import 'package:weather_app/src/resources/translations/codegen_loader.g.dart';
 
+const channel = AndroidNotificationChannel(
+  'high_importance_chanel',
+  'High importance notification',
+  'This chanel is used for important notification',
+  importance: Importance.high,
+);
+
+final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   await EasyLocalization.ensureInitialized();
   await dot_env.dotenv.load();
+
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage((message) async {
+    await Firebase.initializeApp();
+  });
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
   runApp(
     EasyLocalization(
       path: 'assets/translations',
       supportedLocales: const [
-         Locale('en'),
-         Locale('ru'),
+        Locale('en'),
+        Locale('ru'),
       ],
       fallbackLocale: const Locale('en'),
       assetLoader: const CodegenLoader(),
